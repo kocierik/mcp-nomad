8f613e02a5ee81833767ce6451cc3b535aac134c
kocierik <kocierik@gmail.com>
2025-04-24 11:27:32 +0200

    fix: publish script

--- packages/publish_npm.sh
+++ packages/publish_npm.sh
@@ -4,25 +4,25 @@ set -e
 
 # Mac
 mkdir -p ./packages/npm-mcp-nomad-darwin-x64/bin
-cp dist/mcp-nomad-go_darwin_amd64_v1/mcp-nomad-go ./packages/npm-mcp-nomad-darwin-x64/bin/mcp-nomad-go
-chmod +x ./packages/npm-mcp-nomad-darwin-x64/bin/mcp-nomad-go
+cp dist/mcp-nomad-go_darwin_amd64_v1/mcp-nomad ./packages/npm-mcp-nomad-darwin-x64/bin/mcp-nomad
+chmod +x ./packages/npm-mcp-nomad-darwin-x64/bin/mcp-nomad
 mkdir -p ./packages/npm-mcp-nomad-darwin-arm64/bin
-cp dist/mcp-nomad-go_darwin_arm64_v8.0/mcp-nomad-go ./packages/npm-mcp-nomad-darwin-arm64/bin/mcp-nomad-go
-chmod +x ./packages/npm-mcp-nomad-darwin-arm64/bin/mcp-nomad-go
+cp dist/mcp-nomad-go_darwin_arm64_v8.0/mcp-nomad ./packages/npm-mcp-nomad-darwin-arm64/bin/mcp-nomad
+chmod +x ./packages/npm-mcp-nomad-darwin-arm64/bin/mcp-nomad
 
 # Linux
 mkdir -p ./packages/npm-mcp-nomad-linux-x64/bin
-cp dist/mcp-nomad-go_linux_amd64_v1/mcp-nomad-go ./packages/npm-mcp-nomad-linux-x64/bin/mcp-nomad-go
-chmod +x ./packages/npm-mcp-nomad-linux-x64/bin/mcp-nomad-go
+cp dist/mcp-nomad-go_linux_amd64_v1/mcp-nomad ./packages/npm-mcp-nomad-linux-x64/bin/mcp-nomad
+chmod +x ./packages/npm-mcp-nomad-linux-x64/bin/mcp-nomad
 mkdir -p ./packages/npm-mcp-nomad-linux-arm64/bin
-cp dist/mcp-nomad-go_linux_arm64_v8.0/mcp-nomad-go ./packages/npm-mcp-nomad-linux-arm64/bin/mcp-nomad-go
-chmod +x ./packages/npm-mcp-nomad-linux-arm64/bin/mcp-nomad-go
+cp dist/mcp-nomad-go_linux_arm64_v8.0/mcp-nomad ./packages/npm-mcp-nomad-linux-arm64/bin/mcp-nomad
+chmod +x ./packages/npm-mcp-nomad-linux-arm64/bin/mcp-nomad
 
 # Windows
 mkdir -p ./packages/npm-mcp-nomad-win32-x64/bin
-cp dist/mcp-nomad-go_windows_amd64_v1/mcp-nomad-go.exe ./packages/npm-mcp-nomad-win32-x64/bin/mcp-nomad-go.exe
+cp dist/mcp-nomad-go_windows_amd64_v1/mcp-nomad.exe ./packages/npm-mcp-nomad-win32-x64/bin/mcp-nomad.exe
 mkdir -p ./packages/npm-mcp-nomad-win32-arm64/bin
-cp dist/mcp-nomad-go_windows_arm64_v8.0/mcp-nomad-go.exe ./packages/npm-mcp-nomad-win32-arm64/bin/mcp-nomad-go.exe
+cp dist/mcp-nomad-go_windows_arm64_v8.0/mcp-nomad.exe ./packages/npm-mcp-nomad-win32-arm64/bin/mcp-nomad.exe
 
 cd packages/npm-mcp-nomad-darwin-x64
 npm publish --access public
